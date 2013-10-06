073ad79cc7d94fb62fc91fee9409a84dddbc67b2
Chuck Rodacker <Charles_Rodacker@yahoo.com>
2013-10-05 20:44:21 -0600

    using clean up

--- Source/HydropnicDiary.Presentation/App.xaml.cs
+++ Source/HydropnicDiary.Presentation/App.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel.Composition;
-using System.ComponentModel.Composition.Hosting;
+﻿using System.ComponentModel.Composition.Hosting;
 using System.Windows;
 
 namespace HydropnicDiary.Presentation
